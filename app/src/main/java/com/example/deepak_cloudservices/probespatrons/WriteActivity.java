f5472e38582b21f3abc8e0f79adcc3c879291ac3
Harish Reddy <harish.myemails@gmail.com>
2016-09-01 11:50:05 +0530

    added a list view

--- app/src/main/java/com/example/deepak_cloudservices/probespatrons/WriteActivity.java
+++ app/src/main/java/com/example/deepak_cloudservices/probespatrons/WriteActivity.java
@@ -1,11 +1,15 @@
 package com.example.deepak_cloudservices.probespatrons;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -34,7 +38,7 @@ public class WriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
-
+        verifyStoragePermissions(this);
         Button submitbutton = (Button) findViewById(R.id.submit_button);
         submitbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -103,13 +107,38 @@ public class WriteActivity extends AppCompatActivity {
                 .url("http://3creview.i3clogic.com/review/")
                 .post(requestBody)
                 .build();
-
-        Response response = client.newCall(request).execute();
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+        } catch (Exception e) {
+            Log.e("image", e.getMessage());
+        }
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
         Log.d("image", response.body().string());
     }
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    //persmission method.
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have read or write permission
+        int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
     public void pickPhoto(View view) {
         // launch the photo picker
         Intent intent = new Intent();
